e84630e384fca363184aeb5bba0d036496f1f936
DurgaPrasadNaidu <49992126+DurgaPrasadNaidu@users.noreply.github.com>
2026-09-15 13:51:44 -0500

    Add: boats-to-save-people - submission-0

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-0.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+
+        int start=0;
+        int end=people.length-1;
+        int boats=0;
+        while(start<=end){
+
+            if(people[start]+people[end]<=limit){
+                boats++;
+                start++;
+                end--;
+            }else{
+                boats++;
+                end--;
+            }
+        }
+          return boats;
+            }
+          
+}
